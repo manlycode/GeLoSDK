a34b397b4fa32e68c47ca86371b1db464e9aed8f
Chris Rittersdorf <chris@mutuallyhuman.com>
2013-10-18 12:28:50 -0400

    Version: v9.9.11

--- GeLoSDK.framework/Versions/A/Headers/GeLoBeaconManager.h
+++ GeLoSDK.framework/Versions/A/Headers/GeLoBeaconManager.h
@@ -10,7 +10,7 @@
 
 /**
  `GeLoBeaconManager` scans for beacons and also maintains a list of beacons it can find.
-**/
+*/
 @interface GeLoBeaconManager : NSObject<CBCentralManagerDelegate> {
     __block NSMutableDictionary	  *knownBeacons;
     __block NSMutableDictionary   *knownTourBeaconsDict;
@@ -32,18 +32,18 @@
 
 + (GeLoBeaconManager *)sharedInstance;
 
-//---------------------------------------------
-// @name Configuring Scanning
-//---------------------------------------------
+///---------------------------
+/// @name Configuring Scanning
+///---------------------------
 - (void)setFalloff:(NSInteger)falloff forId:(NSUInteger)_id;
 - (void)setDefaultFalloff:(NSInteger)falloff;
 - (void)setDefaultTimeToLive:(NSUInteger)ttl;
 - (void)setDefaultSignalCeiling:(NSInteger)ceiling;
 - (void)includeBetaTours;
 
-//---------------------------------------------
-// @name Scanning Control
-//---------------------------------------------
+///-----------------------
+/// @name Scanning Control
+///-----------------------
 - (void)startScanningForBeacons;
 - (void)resumeScanningForBeacons;
 - (BOOL)isScanning;
@@ -51,9 +51,9 @@
 - (void)stopScanningForBeacons;
 - (void)pauseScanningForBeacons;
 
-//---------------------------------------------
-// @name Known Surroundings
-//---------------------------------------------
+///-------------------------
+/// @name Known Surroundings
+///-------------------------
 - (NSArray *)knownBeacons;
 - (NSArray *)knownTourBeacons;
 - (GeLoBeacon *)nearestBeacon;
@@ -63,15 +63,15 @@
 - (GeLoSite *)currentSite;
 - (NSArray*)findKnownTourBeacons;
 
-//---------------------------------------------
-// @name Information
-//---------------------------------------------
+///------------------
+/// @name Information
+///------------------
 - (GeLoBeaconInfo *)infoForBeaconId:(NSNumber *)beaconId;
 - (NSInteger) siteForBeaconId:(NSInteger) beaconId;
 
-//---------------------------------------------
-// @name Utility Methods
-//---------------------------------------------
+///----------------------
+/// @name Utility Methods
+///----------------------
 - (void)loadTourById:(NSNumber *)tourId;
 - (void)loadSiteById:(NSNumber *)siteId;
 - (void)setCurrentTour:(GeLoTour *)tour;
